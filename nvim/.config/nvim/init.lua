02eaad21cfba98b9a7cb4b5e2479b529d397181f
redshifttt <num@privatevoid.net>
2022-11-12 23:13:17 +0000

    1668294797

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,8 @@
+local is_nightly = vim.version().prerelease
+
+if not is_nightly then
+end
+
 -- If this is executed and an error is thrown packer is not installed.
 local packer_is_installed = function() require('impatient') end
 
