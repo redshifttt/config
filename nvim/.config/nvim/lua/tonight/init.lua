7fbaaaffecf7e8328616708c89486637666dc1e5
redshifttt <num@privatevoid.net>
2022-12-16 14:05:16 +0000

    nvim: tonight - further cleanup

--- nvim/.config/nvim/lua/tonight/init.lua
+++ nvim/.config/nvim/lua/tonight/init.lua
@@ -1,12 +1,8 @@
 -- tonight.nvim: lua colour scheme inspired by tomorrow and spacegray
 local g = require("tonight.groups")
 
-local function highlight(group, styles)
-    vim.api.nvim_set_hl(0, group, styles)
-end
-
 for group, styles in pairs(g) do
-    highlight(group, styles)
+    vim.api.nvim_set_hl(0, group, styles)
 end
 
 require('tonight.config')
