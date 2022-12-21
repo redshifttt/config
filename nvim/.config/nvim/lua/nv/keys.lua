a675b8dc4b000e3dcf10768944446129502ab871
redshifttt <num@privatevoid.net>
2022-12-21 09:44:31 +0000

    nvim: change keymap variable name in keys

--- nvim/.config/nvim/lua/nv/keys.lua
+++ nvim/.config/nvim/lua/nv/keys.lua
@@ -1,37 +1,36 @@
 -- General Keybinds (no plugins)
-local map = vim.keymap.set
+local keymap = vim.keymap
 
 local opts = {
     noremap = true
 }
 
 -- Splits
-map("n", "<C-J>", "<C-W><C-J>", opts)
-map("n", "<C-K>", "<C-W><C-K>", opts)
-map("n", "<C-L>", "<C-W><C-L>", opts)
-map("n", "<C-H>", "<C-W><C-H>", opts)
+keymap.set("n", "<C-J>", "<C-W><C-J>", opts)
+keymap.set("n", "<C-K>", "<C-W><C-K>", opts)
+keymap.set("n", "<C-L>", "<C-W><C-L>", opts)
+keymap.set("n", "<C-H>", "<C-W><C-H>", opts)
 
 -- Arrow keys
-map({"n", "i", "v"}, "<Up>", "<Nop>", opts)
-map({"n", "i", "v"}, "<Down>", "<Nop>", opts)
-map({"n", "i", "v"}, "<Left>", "<Nop>", opts)
-map({"n", "i", "v"}, "<Right>", "<Nop>", opts)
+keymap.set({"n", "i", "v"}, "<Up>", "<Nop>", opts)
+keymap.set({"n", "i", "v"}, "<Down>", "<Nop>", opts)
+keymap.set({"n", "i", "v"}, "<Left>", "<Nop>", opts)
+keymap.set({"n", "i", "v"}, "<Right>", "<Nop>", opts)
 
 -- Quickly open my init.vim then let me reload the config
-map("n", "<leader>v", ":vs $MYVIMRC<cr>", opts)
-map("n", "<F5>", ":source $MYVIMRC<cr>", opts)
+keymap.set("n", "<leader>v", ":vs $MYVIMRC<cr>", opts)
+keymap.set("n", "<F5>", ":source $MYVIMRC<cr>", opts)
 
 -- Center the found search term
-map("n", "n", "nzz", opts)
-map("n", "N", "Nzz", opts)
+keymap.set("n", "n", "nzz", opts)
+keymap.set("n", "N", "Nzz", opts)
 
 -- Similar as the above but for motion keys
-map("n", "<ctrl-d>", "<ctrl-d>zz", opts)
-map("n", "<ctrl-u>", "<ctrl-u>zz", opts)
+keymap.set("n", "<ctrl-d>", "<ctrl-d>zz", opts)
+keymap.set("n", "<ctrl-u>", "<ctrl-u>zz", opts)
 
 -- I don't like accidentally hitting these
-map("n", "H", ":nohlsearch<cr>", opts)
-map("n", "L", "<Nop>", opts)
+keymap.set("n", "H", ":nohlsearch<cr>", opts)
+keymap.set("n", "L", "<Nop>", opts)
 
-map('n', 'gx', '<Cmd>call jobstart(["xdg-open", expand("<cfile>")], {"detach": v:true})<CR>')
-
+keymap.set('n', 'gx', '<Cmd>call jobstart(["xdg-open", expand("<cfile>")], {"detach": v:true})<CR>')
