a24ac2925330c4acf713c5708ee44556eb4d8ba9
redshifttt <num@privatevoid.net>
2023-01-07 12:23:52 +0000

    nvim: lualine: reformat

--- nvim/.config/nvim/lua/lualine/themes/tonight.lua
+++ nvim/.config/nvim/lua/lualine/themes/tonight.lua
@@ -1,40 +1,89 @@
-local darker = require("tonight.colours").background_n1
+local c = require("tonight.colours")
+local darker_than_bg = c.background_n1
 
 return {
     normal = {
-        a = { bg = darker },
-        b = { bg = darker },
-        c = { bg = darker },
-        x = { bg = darker }
+        a = {
+            bg = darker_than_bg,
+        },
+        b = {
+            bg = darker_than_bg,
+        },
+        c = {
+            bg = darker_than_bg,
+        },
+        x = {
+            bg = darker_than_bg,
+        }
     },
     insert = {
-        a = { bg = darker },
-        b = { bg = darker },
-        c = { bg = darker },
-        x = { bg = darker }
+        a = {
+            bg = darker_than_bg,
+        },
+        b = {
+            bg = darker_than_bg,
+        },
+        c = {
+            bg = darker_than_bg,
+        },
+        x = {
+            bg = darker_than_bg,
+        }
     },
     visual = {
-        a = { bg = darker },
-        b = { bg = darker },
-        c = { bg = darker },
-        x = { bg = darker }
+        a = {
+            bg = darker_than_bg,
+        },
+        b = {
+            bg = darker_than_bg,
+        },
+        c = {
+            bg = darker_than_bg,
+        },
+        x = {
+            bg = darker_than_bg,
+        }
     },
     replace = {
-        a = { bg = darker },
-        b = { bg = darker },
-        c = { bg = darker },
-        x = { bg = darker }
+        a = {
+            bg = darker_than_bg,
+        },
+        b = {
+            bg = darker_than_bg,
+        },
+        c = {
+            bg = darker_than_bg,
+        },
+        x = {
+            bg = darker_than_bg,
+        }
     },
     command = {
-        a = { bg = darker },
-        b = { bg = darker },
-        c = { bg = darker },
-        x = { bg = darker }
+        a = {
+            bg = darker_than_bg,
+        },
+        b = {
+            bg = darker_than_bg,
+        },
+        c = {
+            bg = darker_than_bg,
+        },
+        x = {
+            bg = darker_than_bg,
+        }
     },
     inactive = {
-        a = { bg = darker },
-        b = { bg = darker },
-        c = { bg = darker },
-        x = { bg = darker }
+        a = {
+            bg = darker_than_bg,
+        },
+        b = {
+            bg = darker_than_bg,
+        },
+        c = {
+            bg = darker_than_bg,
+        },
+        x = {
+            bg = darker_than_bg,
+        }
     }
 }
