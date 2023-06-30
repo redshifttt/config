8831e79e951289a11fa85774409404f3b24513b3
redshifttt <num@privatevoid.net>
2023-06-30 22:58:36 +0100

    hosts/vesta: refactor configuration.nix

--- hosts/vesta/configuration.nix
+++ hosts/vesta/configuration.nix
@@ -1,11 +1,8 @@
 {
-  config,
   pkgs,
-  callPackage,
   ...
 }: {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
   ];
 
@@ -32,47 +29,49 @@
   i18n.defaultLocale = "en_GB.UTF-8";
   console = {
     font = "Lat2-Terminus16";
-    useXkbConfig = true; # use xkbOptions in tty.
+    useXkbConfig = true;
   };
 
-  services.xserver = {
-    enable = true;
-    layout = "gb";
-    displayManager.defaultSession = "none+i3";
-    desktopManager.xterm.enable = false;
-    windowManager.i3 = {
-      enable = true;
-      extraPackages = with pkgs; [
-        bemenu
-        i3blocks
-        dunst
-        picom
-        networkmanagerapplet
-        xwallpaper
-      ];
-    };
-  };
-
+  # For pipewire usually
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    audio.enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
 
-  services.mullvad-vpn.enable = true;
+  services = {
+    xserver = {
+      enable = true;
+      layout = "gb";
+      displayManager.defaultSession = "none+i3";
+      desktopManager.xterm.enable = false;
+      windowManager.i3 = {
+        enable = true;
+        extraPackages = with pkgs; [
+          bemenu
+          i3blocks
+          dunst
+          picom
+          networkmanagerapplet
+          xwallpaper
+        ];
+      };
+    };
+
+    pipewire = {
+      enable = true;
+      audio.enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
+
+    mullvad-vpn.enable = true;
+
+    openssh.enable = false;
+  };
 
   users.users.sean = {
     isNormalUser = true;
     extraGroups = ["wheel" "libvirtd"];
   };
-
-  environment.systemPackages = with pkgs; [];
-
-  nix.settings.experimental-features = ["nix-command" "flakes"];
 
   programs = {
     mtr.enable = true;
@@ -80,13 +79,12 @@
       enable = true;
       enableSSHSupport = true;
     };
+    dconf.enable = true;
   };
 
-  services.openssh.enable = false;
-
-  programs.dconf.enable = true;
-
   virtualisation.libvirtd.enable = true;
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 
   system.stateVersion = "23.11";
 }
