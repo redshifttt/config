a100fd82c0ec0b71b5f1a97518ec1a17a277c424
redshifttt <num@privatevoid.net>
2023-07-14 19:33:34 +0100

    flake.nix: remove nur input

--- flake.nix
+++ flake.nix
@@ -9,12 +9,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nur.url = "github:nix-community/NUR";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     nil.url = "github:oxalica/nil";
   };
 
-  outputs = { self, nixpkgs, home-manager, nur, ... } @ inputs:
+  outputs = { self, nixpkgs, home-manager, ... } @ inputs:
   let
     pkgs = nixpkgs.legacyPackages.${system};
     system = "x86_64-linux";
