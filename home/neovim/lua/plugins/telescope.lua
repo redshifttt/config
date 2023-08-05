dc17727f86f2838701a6f567c5cc6aea6bbc6c3c
redshifttt <num@privatevoid.net>
2023-08-05 22:59:18 +0100

    home/neovim: find text with telescope

--- home/neovim/lua/plugins/telescope.lua
+++ home/neovim/lua/plugins/telescope.lua
@@ -11,6 +11,7 @@ vim.keymap.set("n", "<leader>fm", function() builtin.man_pages() end, { desc = "
 vim.keymap.set("n", "<leader>ff", function() builtin.find_files() end, { desc = "Find files recursively" })
 vim.keymap.set("n", "<leader>fc", function() builtin.commands() end, { desc = "Find commands" })
 vim.keymap.set("n", "<leader>fb", function() builtin.buffers() end, { desc = "Find buffers" })
+vim.keymap.set("n", "<leader>ft", function() builtin.live_grep() end, { desc = "Find text" })
 
 vim.keymap.set("n", "<leader>gf", function() builtin.git_files() end, { desc = "Find git files" })
 
