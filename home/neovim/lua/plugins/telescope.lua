883daddaa55514bc981cba5b446437e9c7aa90a1
redshifttt <num@privatevoid.net>
2023-08-07 14:12:15 +0100

    home/neovim: remove zk keybinds

--- home/neovim/lua/plugins/telescope.lua
+++ home/neovim/lua/plugins/telescope.lua
@@ -14,38 +14,3 @@ vim.keymap.set("n", "<leader>fb", function() builtin.buffers() end, { desc = "Fi
 vim.keymap.set("n", "<leader>ft", function() builtin.live_grep() end, { desc = "Find text" })
 
 vim.keymap.set("n", "<leader>gf", function() builtin.git_files() end, { desc = "Find git files" })
-
--- Zettelkasten management
-vim.keymap.set("n", "<leader>zF",
-    function()
-        builtin.find_files {
-            prompt_title = "Search for zettels via their file names",
-            results_title = "Zettels",
-        }
-    end,
-{ desc = "Fuzzy find zettels via their file names" })
-
-vim.keymap.set("n", "<leader>zf",
-    function()
-        builtin.live_grep {
-            prompt_title = "Search for zettels via their content",
-            results_title = "Zettels",
-            -- default_mappings = {
-            --     i = {
-            --         ["CTRL_F"] = "<cmd>lua vim.fn.expand(\"%\")<cr>"
-            --     }
-            -- }
-        }
-    end,
-{ desc = "Fuzzy find zettels via their file contents" })
-
-vim.keymap.set("n", "<leader>zl",
-    function()
-        local file_name = vim.fn.expand("%:t")
-        builtin.grep_string {
-            prompt_title = "Search for zettels that link here",
-            results_title = "Zettels",
-            search = file_name,
-        }
-    end,
-{ desc = "Fuzzy find zettels that link to current opened zettel" })
