24c6ae967bf0bd6877af939efd3c8cd27249e606
redshifttt <num@privatevoid.net>
2023-08-13 00:43:07 +0100

    home/firefox: move firefox cfg

--- home/default.nix
+++ home/default.nix
@@ -17,6 +17,7 @@
     ./polybar
     ./git
     ./newsboat
+    ./firefox
   ];
 
   home = {
@@ -29,7 +30,6 @@
       bat
       lsd
       firefox
-      librewolf
       git
       terminus_font
       cantarell-fonts
@@ -71,41 +71,5 @@
     enable = true;
     enableZshIntegration = true;
     nix-direnv.enable = true;
-  };
-
-  programs.firefox = {
-    enable = true;
-    profiles."default" = {
-      name = "default";
-      isDefault = true;
-      search = {
-        default = "DuckDuckGo";
-        force = true;
-        order = [ "DuckDuckGo" ];
-        # TODO: refactor the below to be more dynamic.
-        engines = {
-          "DuckDuckGo" = {
-            urls = [{ template = "https://duckduckgo.com/?q={searchTerms}"; }];
-          };
-          "Google".metaData.hidden = true;
-          "Amazon.co.uk".metaData.hidden = true;
-          "Bing".metaData.hidden = true;
-          "eBay".metaData.hidden = true;
-          "Wikipedia (en)".metaData.hidden = true;
-        };
-      };
-      extraConfig = let
-        arkenfox = pkgs.fetchurl {
-          url = "https://raw.githubusercontent.com/arkenfox/user.js/master/user.js";
-          hash = "sha256-sysEtq4aEWmkKy3KPe+4J/HJxjCxNcTAzptZ7s5JrJg=";
-        };
-      in "${lib.strings.fileContents arkenfox}";
-      extensions = with inputs.self.packages.x86_64-linux.firefox-addons; [
-        ublock-origin
-        violentmonkey
-        bitwarden
-        stylus
-      ];
-    };
   };
 }
---
+++ home/firefox/default.nix
@@ -0,0 +1,43 @@
+{ pkgs, lib, inputs, ... }:
+{
+  programs.firefox = {
+    enable = true;
+    profiles."default" = {
+      name = "default";
+      isDefault = true;
+      search = {
+        default = "DuckDuckGo";
+        force = true;
+        order = [ "DuckDuckGo" ];
+        # TODO: refactor the below to be more dynamic.
+        engines = {
+          "DuckDuckGo" = {
+            urls = [{ template = "https://duckduckgo.com/?q={searchTerms}"; }];
+          };
+          "Google".metaData.hidden = true;
+          "Amazon.co.uk".metaData.hidden = true;
+          "Bing".metaData.hidden = true;
+          "eBay".metaData.hidden = true;
+          "Wikipedia (en)".metaData.hidden = true;
+        };
+      };
+      extraConfig = let
+        arkenfox = pkgs.fetchurl {
+          url = "https://raw.githubusercontent.com/arkenfox/user.js/master/user.js";
+          hash = "sha256-sysEtq4aEWmkKy3KPe+4J/HJxjCxNcTAzptZ7s5JrJg=";
+        };
+      in ''
+      ${lib.strings.fileContents arkenfox}
+
+      // ffprofile additions -- may contain duplicates
+      ${lib.strings.fileContents ./ffprofile.js}
+      '';
+      extensions = with inputs.self.packages.x86_64-linux.firefox-addons; [
+        ublock-origin
+        violentmonkey
+        bitwarden
+        stylus
+      ];
+    };
+  };
+}
