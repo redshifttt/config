e80c4e4194cfee91a176164450f76b2bcba9a1b2
redshifttt <num@privatevoid.net>
2023-08-18 18:55:45 +0100

    home/neovim: more telescope config

--- home/neovim/lua/plugins/telescope.lua
+++ home/neovim/lua/plugins/telescope.lua
@@ -1,13 +1,28 @@
 local telescope = require('telescope')
 local builtin = require('telescope.builtin')
 
-telescope.setup {}
+telescope.setup {
+    defaults = {
+        layout_strategy = 'center',
+        layout_config = { anchor = "CENTER" },
+        vimgrep_arguments = {
+          "rg",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+          "--trim"
+        }
+    },
+}
 
 vim.keymap.set("n", "<leader>t", "<cmd>Telescope<cr>")
 
 vim.keymap.set("n", "<leader>fh", function() builtin.help_tags() end, { desc = "Find help tags" })
 vim.keymap.set("n", "<leader>fm", function() builtin.man_pages() end, { desc = "Find man pages" })
-vim.keymap.set("n", "<leader>ff", function() builtin.find_files() end, { desc = "Find files recursively" })
+vim.keymap.set("n", "<leader>ff", function() builtin.find_files() end, { desc = "Find files" })
 vim.keymap.set("n", "<leader>fc", function() builtin.commands() end, { desc = "Find commands" })
 vim.keymap.set("n", "<leader>fb", function() builtin.buffers() end, { desc = "Find buffers" })
 vim.keymap.set("n", "<leader>ft", function() builtin.live_grep() end, { desc = "Find text" })
