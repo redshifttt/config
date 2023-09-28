87b3eef294ce9238c1955aecaf1f7184a110862d
redshifttt <num@privatevoid.net>
2023-09-28 18:20:43 +0100

    packages/firefox-addons: remove versioning of addons

--- packages/firefox-addons/default.nix
+++ packages/firefox-addons/default.nix
@@ -1,29 +1,11 @@
 { pkgs, stdenv, ... }:
 
-# let
-#   buildFirefoxXpiAddon = pname: version: src: guid:
-#     stdenv.mkDerivation {
-#       inherit pname;
-#       inherit version;
-#       inherit src;
-#
-#       dontUnpack = true;
-#       preferLocalBuild = true;
-#       allowSubstitutes = true;
-#
-#       buildCommand = ''
-#       dst="$out/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}"
-#       mkdir -p "$dst"
-#       install -v -m644 "$src" "$dst/${guid}.xpi"
-#       '';
-#     };
-# in
 {
-  ublock-origin = stdenv.mkDerivation rec {
+  ublock-origin = stdenv.mkDerivation {
     pname = "ublock_origin";
-    version = "1.51.0";
+    version = "latest";
     src = pkgs.fetchurl {
-      url = "https://addons.mozilla.org/firefox/downloads/file/4141256/${pname}-${version}.xpi";
+      url = "https://addons.mozilla.org/firefox/downloads/file/4141256";
       hash = "sha256-i3NGi8IzoR3SiVIZRmOBeD0ZEjhX3Qtv0WoBgg/KSDQ=";
     };
 
@@ -32,16 +14,16 @@
     allowSubstitutes = true;
 
     buildCommand = ''
-    dst="$out/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}"
-    mkdir -p "$dst"
-    install -v -m644 "$src" "$dst/uBlock0@raymondhill.net.xpi"
+      dst="$out/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}"
+      mkdir -p "$dst"
+      install -v -m644 "$src" "$dst/uBlock0@raymondhill.net.xpi"
     '';
   };
-  violentmonkey = stdenv.mkDerivation rec {
+  violentmonkey = stdenv.mkDerivation {
     pname = "violentmonkey";
-    version = "2.15.0";
+    version = "latest";
     src = pkgs.fetchurl {
-      url = "https://addons.mozilla.org/firefox/downloads/file/4142251/${pname}-${version}.xpi";
+      url = "https://addons.mozilla.org/firefox/downloads/file/4142251";
       hash = "sha256-iU5Uy+nc0jXervBUuSaKlV/tmv7hVuvUIknCsWHFU1I=";
     };
 
@@ -50,16 +32,16 @@
     allowSubstitutes = true;
 
     buildCommand = ''
-    dst="$out/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}"
-    mkdir -p "$dst"
-    install -v -m644 "$src" "$dst/{aecec67f-0d10-4fa7-b7c7-609a2db280cf}.xpi"
+      dst="$out/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}"
+      mkdir -p "$dst"
+      install -v -m644 "$src" "$dst/{aecec67f-0d10-4fa7-b7c7-609a2db280cf}.xpi"
     '';
   };
-  bitwarden = stdenv.mkDerivation rec {
-    pname = "bitwarden_password_manager";
-    version = "2023.7.1";
+  bitwarden = stdenv.mkDerivation {
+    pname = "bitwarden";
+    version = "latest";
     src = pkgs.fetchurl {
-      url = "https://addons.mozilla.org/firefox/downloads/file/4140393/${pname}-${version}.xpi";
+      url = "https://addons.mozilla.org/firefox/downloads/file/4140393";
       hash = "sha256-ozlx2A9VInn5ZXl4aTiujojEuVwp+DOcQvXeR/FSAK0=";
     };
 
@@ -68,9 +50,27 @@
     allowSubstitutes = true;
 
     buildCommand = ''
-    dst="$out/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}"
-    mkdir -p "$dst"
-    install -v -m644 "$src" "$dst/{446900e4-71c2-419f-a6a7-df9c091e268b}.xpi"
+      dst="$out/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}"
+      mkdir -p "$dst"
+      install -v -m644 "$src" "$dst/{446900e4-71c2-419f-a6a7-df9c091e268b}.xpi"
+    '';
+  };
+  darkreader = stdenv.mkDerivation {
+    pname = "darkreader";
+    version = "latest";
+    src = pkgs.fetchurl {
+      url = "https://addons.mozilla.org/firefox/downloads/file/4172671";
+      hash = "sha256-ILUzVsNrDHbfYU4suE5/8+GrdbT+L9K7ygOQJtAYgT8=";
+    };
+
+    dontUnpack = true;
+    preferLocalBuild = true;
+    allowSubstitutes = true;
+
+    buildCommand = ''
+      dst="$out/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}"
+      mkdir -p "$dst"
+      install -v -m644 "$src" "$dst/addon@darkreader.org.xpi"
     '';
   };
 }
