9e29ad3090a1fab8e04e37a8ea68b977f1e1a362
redshifttt <num@privatevoid.net>
2023-09-30 19:21:28 +0100

    home/firefox: user.js changes

--- home/firefox/default.nix
+++ home/firefox/default.nix
@@ -10,15 +10,10 @@
         force = true;
         order = [ "DuckDuckGo" ];
         # TODO: refactor the below to be more dynamic.
-        engines = {
+        engines = lib.mkForce {
           "DuckDuckGo" = {
             urls = [{ template = "https://duckduckgo.com/?q={searchTerms}"; }];
           };
-          "Google".metaData.hidden = true;
-          "Amazon.co.uk".metaData.hidden = true;
-          "Bing".metaData.hidden = true;
-          "eBay".metaData.hidden = true;
-          "Wikipedia (en)".metaData.hidden = true;
         };
       };
       extraConfig = let
@@ -26,31 +21,32 @@
           url = "https://raw.githubusercontent.com/arkenfox/user.js/master/user.js";
           hash = "sha256-sysEtq4aEWmkKy3KPe+4J/HJxjCxNcTAzptZ7s5JrJg=";
         };
-      in ''
-      ${lib.strings.fileContents arkenfox}
+        in ''
+        ${lib.strings.fileContents arkenfox}
 
-      // custom arkenfox changes
-      /// disable safe browsing (sends urls to google)
-      user_pref("browser.safebrowsing.malware.enabled", false);
-      user_pref("browser.safebrowsing.phishing.enabled", false);
-      user_pref("browser.safebrowsing.downloads.enabled", false);
-      user_pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", false);
-      user_pref("browser.safebrowsing.downloads.remote.block_uncommon", false);
-      /// disable DoH
-      user_pref("network.trr.mode", 5);
-      user_pref("network.trr.uri", "");
-      user_pref("network.trr.custom_uri", "");
-      /// enable webgl so everything doesnt fucking break
-      user_pref("webgl.disabled", false);
+        // custom arkenfox changes
+        /// disable safe browsing (sends urls to google)
+        user_pref("browser.safebrowsing.malware.enabled", false);
+        user_pref("browser.safebrowsing.phishing.enabled", false);
+        user_pref("browser.safebrowsing.downloads.enabled", false);
+        user_pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", false);
+        user_pref("browser.safebrowsing.downloads.remote.block_uncommon", false);
+        /// disable DoH
+        user_pref("network.trr.mode", 5);
+        user_pref("network.trr.uri", "");
+        user_pref("network.trr.custom_uri", "");
+        /// enable webgl so everything doesnt fucking break
+        user_pref("webgl.disabled", false);
 
-      // not in arkenfox
-      user_pref("extensions.pocket.enabled", false);
-      user_pref("browser.urlbar.suggest.pocket", false);
-      '';
+        // not in arkenfox
+        user_pref("extensions.pocket.enabled", false);
+        user_pref("browser.urlbar.suggest.pocket", false);
+        '';
       extensions = with inputs.self.packages.x86_64-linux.firefox-addons; [
         ublock-origin
         violentmonkey
         bitwarden
+        darkreader
       ];
     };
   };
