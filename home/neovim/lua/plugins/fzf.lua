a15c63619a692f7f1e6bcba6a390cecbdbab5995
redshifttt <num@privatevoid.net>
2023-10-09 17:19:14 +0100

    home/neovim: change fzf git_files keybind

--- home/neovim/lua/plugins/fzf.lua
+++ home/neovim/lua/plugins/fzf.lua
@@ -1,11 +1,10 @@
 local fzf = require("fzf-lua")
 
 vim.keymap.set("n", "<leader>ff", function() fzf.files({ cmd = "bfs" }) end)
+vim.keymap.set("n", "<leader>fg", function() fzf.git_files() end)
 vim.keymap.set("n", "<leader>fh", function() fzf.help_tags() end)
 vim.keymap.set("n", "<leader>fH", function() fzf.highlights() end)
 vim.keymap.set("n", "<leader>fc", function() fzf.commands() end)
 vim.keymap.set("n", "<leader>fb", function() fzf.buffers() end)
 vim.keymap.set("n", "<leader>ft", function() fzf.live_grep() end)
 vim.keymap.set("n", "<leader>fo", function() fzf.oldfiles() end)
-
-vim.keymap.set("n", "<leader>gf", function() fzf.git_files() end)
