03cf17cb6a9e754803df8e6a7b7c3a237ade72ed
redshifttt <num@privatevoid.net>
2023-10-09 17:19:53 +0100

    home/neovim: migrate indent-blankline config to v3

--- home/neovim/lua/plugins/other.lua
+++ home/neovim/lua/plugins/other.lua
@@ -8,11 +8,16 @@ toggleterm.setup({ shade_terminals = false })
 vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm<cr>")
 
 -- indent-blankline
-vim.g.indent_blankline_filetype_exclude = {
-    'help',
-    'man',
-    'glowpreview'
-}
+require("ibl").setup({
+    config = {
+        indent = {
+            char = "▏"
+        },
+        exclude = {
+            filetypes = { 'glowpreview' }
+        }
+    }
+})
 
 -- oil
 vim.keymap.set("n", "<leader>o", function() require("oil").open() end)
