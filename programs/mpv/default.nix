357b3e3e26c3bbb08ba38726d0dd74b87e067027
redshifttt <num@privatevoid.net>
2023-11-30 13:33:31 +0000

    programs/mpv: change keybinds

--- programs/mpv/default.nix
+++ programs/mpv/default.nix
@@ -2,25 +2,31 @@
   programs.mpv = {
     enable = true;
     bindings = {
-      LEFT = "seek -10";
-      RIGHT = "seek 10";
-      UP = "playlist-prev";
-      DOWN = "playlist-next";
-      SPACE = "cycle pause";
+      k = "seek -5";
+      j = "seek 5";
+      h = "playlist-prev";
+      l = "playlist-next";
       m = "cycle mute";
-      "<" = "no-osd cycle video-rotate 90";
-      ">" = "no-osd cycle video-rotate 90";
+      SPACE = "cycle pause";
       "." = "show-progress";
     };
     config = {
       msg-module = true;
       msg-color = true;
-      osd-font = "Noto Sans";
+      msg-time = true;
+
+      osd-font = "Cantarell";
       osd-font-size = 15;
       osd-border-size = 2;
       osd-duration = 3500;
+      osd-bar-align-x = 0;
+      osd-bar-align-y = 1;
+      osd-bar-w = 100;
+      osd-bar-h = 2;
+
       user-agent = "Mozilla/5.0";
       hls-bitrate = "max";
+
       sub-font = "Source Sans Pro";
       sub-font-size = 36;
       sub-color = "1.0/1.0/1.0/1.0";
