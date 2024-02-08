929ced9fd49e371ed9c96664c5b99fb030302139
redshifttt <num@privatevoid.net>
2024-02-08 21:51:28 +0000

    programs/neovim: remove useless treeshitter config

--- programs/neovim/lua/plugins/treesitter.lua
+++ programs/neovim/lua/plugins/treesitter.lua
@@ -10,41 +10,4 @@ treesitter.setup {
     indent = {
         enable = true,
     },
-    playground = {
-        enable = true,
-        disable = {},
-        updatetime = 10, -- Debounced time for highlighting nodes in the playground from source code
-        persist_queries = false, -- Whether the query persists across vim sessions
-        keybindings = {
-            toggle_query_editor = 'o',
-            toggle_hl_groups = 'i',
-            toggle_injected_languages = 't',
-            toggle_anonymous_nodes = 'a',
-            toggle_language_display = 'I',
-            focus_language = 'f',
-            unfocus_language = 'F',
-            update = 'R',
-            goto_node = '<cr>',
-            show_help = '?',
-        },
-    },
-    textobjects = {
-        select = {
-            enable = true,
-            lookahead = false,
-            keymaps = {
-                -- You can use the capture groups defined in textobjects.scm
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                ["ic"] = "@class.inner",
-            },
-            selection_modes = {
-                ['@parameter.outer'] = 'v', -- charwise
-                ['@function.outer'] = 'V', -- linewise
-                ['@class.outer'] = '<c-v>', -- blockwise
-            },
-            include_surrounding_whitespace = false,
-        },
-    },
 }
