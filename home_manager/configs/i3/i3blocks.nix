dd75c7a844a593d0f79adaf25cbc1be86b8f0623
redshifttt <num@privatevoid.net>
2024-02-24 17:53:53 +0000

    configs/i3: add disk usage to i3blocks

--- home_manager/configs/i3/i3blocks.nix
+++ home_manager/configs/i3/i3blocks.nix
@@ -8,10 +8,16 @@
         separator = true;
         separator_block_width = 15;
       };
-      sections.time = { # do you really need more? lol
-        command = "date +'%F (%a) %T %Z'";
-        interval = 1;
+      sections = {
+        rootsize = { # do you really need more? lol
+          command = "df -h --output=\"avail,pcent\" / | sed 1d | awk '{printf \"/: %s left (%s used)\\n\", $1, $2}'";
+          interval = 1;
         };
+        time = { # do you really need more? lol
+          command = "date +'%F (%a) %T %Z'";
+          interval = 1;
+        };
+      };
     };
   };
 }
