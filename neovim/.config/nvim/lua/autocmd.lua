a324688f4cd2a09d6c194d2df5ff629889c5aa90
redshifttt <num@privatevoid.net>
2024-04-16 16:11:54 +0100

    neovim: add yaml to 2 space tab autocmd

--- neovim/.config/nvim/lua/autocmd.lua
+++ neovim/.config/nvim/lua/autocmd.lua
@@ -20,7 +20,7 @@ vim.api.nvim_create_autocmd("FileType", {
     end
 })
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "nix",
+    pattern = { "nix", "yaml" },
     callback = function()
         vim.opt.tabstop = 2
         vim.opt.shiftwidth = 2
