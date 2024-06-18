6e16fdc61e76a4e1b3b629690caec15f29f8c71a
redshifttt <num@privatevoid.net>
2024-06-18 14:06:17 +0100

    home_manager: `with` cleanup

--- home_manager/common.nix
+++ home_manager/common.nix
@@ -1,4 +1,11 @@
 { pkgs, inputs, ... }:
+
+let
+  inherit (builtins) attrValues;
+
+  username = "sean";
+  homeDirectory = "/home/${username}";
+in
 {
   imports = [
     ./configs/neovim
@@ -19,54 +26,56 @@
     ./configs/feh
   ];
 
-  home = rec {
-    username = "sean";
-    homeDirectory = "/home/${username}";
+  home = {
+    inherit username homeDirectory;
     stateVersion = "23.11";
 
     packages = let
-      cliPrograms = with pkgs; [
-        bat
-        lsd
-        ripgrep
-        bfs
-        nil
-        htop
-        yt-dlp
-        pet
-        jq
-        mpc-cli
-        file
-        unzip
-        nvd
-        wget
-        nix-search-cli
-      ];
-      guiPrograms = with pkgs; [
-        easyeffects
-        ckan
-        audacity
-        pavucontrol
-        thunderbird
-        mangohud
-        prismlauncher
-        spotify
-        libqalculate
-        bitwarden
-        gparted
-        librewolf
-        ckan
-      ];
-      fontPackages = with pkgs; [
-        cantarell-fonts
-        liberation_ttf
-        terminus_font
-        dejavu_fonts
-        open-sans
-      ];
-      customPackages = with inputs.self.packages.x86_64-linux; [
-        fantasque-sans-mono
-      ];
+      cliPrograms = attrValues {
+        inherit (pkgs)
+          bat
+          lsd
+          ripgrep
+          bfs
+          nil
+          htop
+          yt-dlp
+          pet
+          jq
+          mpc-cli
+          file
+          unzip
+          nvd
+          wget
+          nix-search-cli;
+      };
+      guiPrograms = attrValues {
+        inherit (pkgs)
+          easyeffects
+          ckan
+          audacity
+          pavucontrol
+          thunderbird
+          # mangohud
+          prismlauncher
+          spotify
+          libqalculate
+          bitwarden
+          gparted
+          librewolf;
+      };
+      fontPackages = attrValues {
+        inherit (pkgs)
+          cantarell-fonts
+          liberation_ttf
+          terminus_font
+          dejavu_fonts
+          open-sans;
+      };
+      customPackages = attrValues {
+        inherit (inputs.self.packages.x86_64-linux)
+          fantasque-sans-mono;
+      };
     in []
       ++ cliPrograms
       ++ guiPrograms
