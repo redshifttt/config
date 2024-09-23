3b75b861a7f5edfeadcaa18ac7d9cb2ed7621b6a
redshifttt <num@privatevoid.net>
2024-09-23 21:34:18 +0100

    packages/go-fts: add recursion option

---
+++ packages/go-fts/README.md
@@ -0,0 +1,10 @@
+# go-fts
+
+Search through a file system and count up all the file types.
+
+![example screenshot](example-screenshot.png)
+
+Pretty proud of this little thing.
+
+By default searches current directory but with `-r` will do recursive search. A
+few bugs with recursive, such as not filtering out hidden stuff, but all round pretty neato.
--- packages/go-fts/main.go
+++ packages/go-fts/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
-    "sort"
-    "strconv"
-    "strings"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+	"text/tabwriter"
 )
+
+var entries []fs.DirEntry
 
 type FileType struct {
     Extension  string
@@ -31,11 +35,32 @@ func humanizeUnits(units int64) string {
     return fmt.Sprintf("%d %s", units, siUnits[unitCombo])
 }
 
+func grabFilepathBasename(path string, entry fs.DirEntry, err error) error {
+    entries = append(entries, entry)
+    return nil
+}
+
 func main() {
-    cwd, _ := os.Getwd()
-    entries, err := os.ReadDir(cwd)
+    var err error
+
+    cwd, err := os.Getwd()
+
     if err != nil {
         log.Fatalln("error: ", err)
+    }
+
+    if len(os.Args) > 1 {
+        if os.Args[1:][0] == "-r" {
+            err = filepath.WalkDir(cwd, grabFilepathBasename)
+            if err != nil {
+                log.Fatalln("error: ", err)
+            }
+        }
+    } else {
+        entries, err = os.ReadDir(cwd)
+        if err != nil {
+            log.Fatalln("error: ", err)
+        }
     }
 
     fileTypes := make([]FileType, 0)
@@ -51,7 +76,7 @@ func main() {
             continue
         }
 
-        filename := f.Name()
+        filename := filepath.Base(f.Name())
 
         if strings.HasPrefix(filename, ".") {
             // hiddenCount += 1
@@ -99,48 +124,14 @@ func main() {
     fmt.Printf("Total: %d", fileCount)
     fmt.Printf(" (%s)\n", humanizeUnits(allBytesCount))
 
-    tblHeader := []string{"File type", "Count", "Total culminative size"}
-    maxPadding := []int{0, 0, 0}
+    w := tabwriter.NewWriter(os.Stdout, 1, 1, 2, ' ', 0)
 
-    for _, v := range fileTypes {
-        if len(v.Extension) >= maxPadding[0] {
-            maxPadding[0] = len(v.Extension)
-        }
-        if len(strconv.Itoa(v.Count)) >= maxPadding[1] {
-            maxPadding[1] = len(strconv.Itoa(v.Count))
-        }
-    }
-
-    if maxPadding[0] <= len(tblHeader[0]) {
-        maxPadding[0] = len(tblHeader[0])
-    }
-    if maxPadding[1] <= len(tblHeader[1]) {
-        maxPadding[1] = len(tblHeader[1])
-    }
-
-    for _, v := range tblHeader {
-        fmt.Printf("\x1b[1m%s ", v)
-    }
-    fmt.Printf("\x1b[0m\n")
+    fmt.Fprintf(w, "%s\t%s\t%s\n", "File type", "Count", "Total culminative size")
 
     // I guess this mess works?
     for _, v := range fileTypes {
-        formattedLine := ""
-        formattedLine += "%-"
-        formattedLine += strconv.Itoa(maxPadding[0])
-        formattedLine += "s"
-
-        formattedLine += " "
-
-        formattedLine += "%-"
-        formattedLine += strconv.Itoa(maxPadding[1])
-        formattedLine += "d"
-
-        formattedLine += " "
-
-        formattedLine += "%s"
-        formattedLine += "\n"
-
-        fmt.Printf(formattedLine, v.Extension, v.Count, humanizeUnits(v.BytesCount))
+        fmt.Fprintf(w, "%s\t%d\t%s\n", v.Extension, v.Count, humanizeUnits(v.BytesCount))
     }
+
+    w.Flush()
 }
