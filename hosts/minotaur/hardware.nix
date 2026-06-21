5504d3ef91505424fe4df76665b7e4d155743dc1
redshifttt <num@privatevoid.net>
2026-06-21 22:41:08 +0100

    new hardware

--- hosts/minotaur/hardware.nix
+++ hosts/minotaur/hardware.nix
@@ -8,34 +8,25 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "usbhid" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/111f9feb-36ac-42b2-9040-fd22d1d9aadc";
-      fsType = "xfs";
+    { device = "/dev/mapper/root";
+      fsType = "btrfs";
     };
 
-  boot.initrd.luks.devices."root".device = "/dev/disk/by-uuid/329c13b1-b637-4c14-a746-18a4b3313944";
+  boot.initrd.luks.devices."root".device = "/dev/disk/by-uuid/9d9d87a1-c4c6-49e7-b89e-5315fa7456b3";
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/4C2C-A23D";
+    { device = "/dev/disk/by-uuid/9D30-FA5E";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
   swapDevices = [ ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp7s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.virbr0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp6s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
